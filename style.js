8c2b7eeafaa1616ed867357f48c61a6cea6fc457
dohabristy <labornodb@gmail.com>
2024-02-02 18:51:02 -0500

    done

---
+++ style.js
@@ -0,0 +1,16 @@
+let dark_btn = document.querySelector(".dark-btn")
+
+
+dark_btn.addEventListener("click",function(){
+   document.body.classList.toggle("dark") 
+})
+
+let toggle = document.querySelector(".toggle")
+
+toggle.addEventListener("click",function(){
+   toggle.classList.toggle("active")
+})
+
+function rng(min,max){
+   
+}
